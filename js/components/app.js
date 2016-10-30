90cb1ea2fb4e0778ae1a3cc0a3c75446e5e16730
Lizzie Paris <lzz.paris@gmail.com>
2016-10-29 17:52:48 -0700

    Log TasteKidForm input to console

--- js/components/app.js
+++ js/components/app.js
@@ -1,8 +1,15 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+var TasteKidForm = require("./taste-kid-form");
+
 var App = function(){
-  return (<p>Hello world!</p>);
+  return (
+    <div>
+      <h1>Artist Hunter</h1>    
+      <TasteKidForm />
+    </div>
+  );
 };
 
 module.exports = App;
