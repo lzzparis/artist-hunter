6f94ee9bf8e1d57713bc3b9483944762e8cf9707
Lizzie Paris <lzz.paris@gmail.com>
2016-11-03 17:35:42 -0700

    WIP - styling

--- js/reducers/app-reducer.js
+++ js/reducers/app-reducer.js
@@ -7,9 +7,22 @@ var initialState = {
   seedArtistId: null,
   topResultClass: "hideTopResult",
   numRecommendations:4,
-  recommendations: []
+  recommendations: [ ]
 };
 
+
+var initialStateForStyling = {
+  seedArtistName: "Mott the Hoople",
+  seedArtistId: 12345678,
+  topResultClass: "showTopResult",
+  numRecommendations:4,
+  recommendations: [
+    {artist:"Something1", images: ["http://placekitten/400/400"]},
+    {artist:"Something2", images: ["http://placekitten/400/400"]},
+    {artist:"Something3", images: ["http://placekitten/400/400"]},
+    {artist:"Something4", images: ["http://placekitten/400/400"]},
+  ]
+} 
 
 var Artist = function(artistInput){
   this.name = artistInput.name;
@@ -18,7 +31,8 @@ var Artist = function(artistInput){
 }
 
 var appReducer = function(state, action){
-  state = state || initialState;
+  // state = state || initialState;
+  state = state || initialStateForStyling;
   if(action.type === actions.FETCH_ARTIST_ID_SUCCESS){
     return Object.assign( {}, state, 
                           {seedArtistName: action.artistName}, 
