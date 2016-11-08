afbdaeea7645cddb49f3e4a4be2c9e8075550c33
Lizzie Paris <lzz.paris@gmail.com>
2016-11-07 18:58:50 -0800

    Style Help component

--- js/components/help.js
+++ js/components/help.js
@@ -2,11 +2,13 @@ var React = require("react");
 
 var Help = function(){
   return (
-    <div className="help-modal">
-      <h3>What do I do with this?</h3>
-      <p> Easy, just search for a musician and you'll get a random selection of related artists 
-          from Spotify, as well as a playlist of their top songs.
-      </p>
+    <div className="help-modal-container">
+      <div className="help-modal">
+        <h3>What do I do?</h3>
+        <p> Easy, just search for a musician and you'll get a random selection of related artists 
+            from Spotify, as well as a playlist of their top songs.
+        </p>
+      </div>
     </div>
     
   );
