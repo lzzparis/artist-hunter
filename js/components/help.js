4c308a9786a435622c496954b6462bd1003c9a64
Lizzie Paris <lzz.paris@gmail.com>
2016-12-21 00:33:33 -0800

    Fix modal for responsiveness, add link to portfolio

--- js/components/help.js
+++ js/components/help.js
@@ -14,6 +14,7 @@ var Help = React.createClass({
           <p>Easy, just search for a musician and you'll get a random selection of related artists 
               from Spotify, as well as a playlist of their top songs.
           </p>
+          <p>Created by <a href="http://lizzieparis.herokuapp.com/" target="_blank">Lizzie Paris</a>.</p>
         </div>
       </div>
     );
